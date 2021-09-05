d4a2793bfd5f07a0ad652aefb58ae1e2ffd22d3a
Humberto <humbertoodantas@gmail.com>
2021-09-05 17:56:43 -0300

    Get localStorage notification

--- front-end/src/js/notification.js
+++ front-end/src/js/notification.js
@@ -2,10 +2,11 @@ const urlParams9 = new URLSearchParams(window.location.search);
 const urlParam9 = urlParams9.get('p');
 const urlParam8 = urlParams9.get('q');
 
-if(urlParam9 == "pu") {
+if(urlParam9 == "pu" && window.localStorage.getItem("p") == "pu") {
   setTimeout(function() {
     document.querySelector(".not").classList.remove("notificationhide");
     document.querySelector(".spncont").innerHTML="Senha alterada com sucesso!";
+    window.localStorage.setItem("p", '');
 
     setTimeout(function() {
       document.querySelector(".not").classList.add("notificationhide");
@@ -14,10 +15,11 @@ if(urlParam9 == "pu") {
   }, 1000);
 }
 
-if(urlParam8 == "px") {
+if(urlParam8 == "px" && window.localStorage.getItem("q") == "px") {
   setTimeout(function() {
     document.querySelector(".not").classList.remove("notificationhide");
     document.querySelector(".spncont").innerHTML="Reserva cancelada com sucesso!";
+    window.localStorage.setItem("q", '');
 
     setTimeout(function() {
       document.querySelector(".not").classList.add("notificationhide");
