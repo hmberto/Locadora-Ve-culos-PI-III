0f4c111d948ad4d0ffb3a6b2cfddc6e909abe098
Humberto <humbertoodantas@gmail.com>
2021-09-19 19:04:11 -0300

    Email Confirmation added

--- back-end/src/main/java/br/com/interdisciplinar/locadora/database/EmailConfirmationDB.java
+++ back-end/src/main/java/br/com/interdisciplinar/locadora/database/EmailConfirmationDB.java
@@ -1,6 +1,7 @@
 package br.com.interdisciplinar.locadora.database;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,29 +18,52 @@ public class EmailConfirmationDB {
 		LOG.entering(NAME, "confirmation");
 		
 		String sql = EnvVariables.getEnvVariable("DATABASE_CONFIRM_EMAIL");
+		String sql1 = EnvVariables.getEnvVariable("DATABASE_VALIDATE_CONFIRM_EMAIL");
 		
 		try {
-			PreparedStatement statement = Database.connect().prepareStatement(sql);
-			statement.setString(1, email.getEmailToken());
-			statement.setString(2, email.getEmail());
+			PreparedStatement statement1 = Database.connect().prepareStatement(sql1);
+			statement1.setString(1, email.getEmail());
 			
-			statement.execute();
-			statement.close();
+			ResultSet f = statement1.executeQuery();
 			
-			LOG.log(Level.INFO, "Successfully confirmation - E-mail: " + email.getEmail());
+			boolean validate = false;
+			while(f.next()) {
+				LOG.log(Level.INFO, "" + f.getBoolean(24));
+				LOG.log(Level.INFO, f.getString(25));
+				if(f.getBoolean(24) == false && f.getString(25).equals(email.getEmailToken())) {
+					validate = true;
+				}
+			}
 			
-			String messageSubject = "Locadora de Veículos BH - E-mail confirmado";
+			LOG.log(Level.INFO, "\n\nValidate: " + validate);
 			
-			String messageText = "Obrigado por confirmar sua conta. \n\n"
-					+ "Faça login e realize a reserva do seu veículo agora mesmo. \n\n"
-					+ "http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com"
-					+ "\n\nVocê pode reservar no site e retirar na agência mais próxima de você. \nConfira as melhores condições para você reservar e aproveitar!";
-			
-			EmailConfirmation sendEmail = new EmailConfirmation();
-			sendEmail.confirmation(email.getEmail(), messageSubject, messageText);
-			
-			LOG.exiting(NAME, "confirmation");
-			return true;
+			if(validate) {
+				PreparedStatement statement = Database.connect().prepareStatement(sql);
+				statement.setString(1, email.getEmailToken());
+				statement.setString(2, email.getEmail());
+				
+				statement.execute();
+				statement.close();
+				
+				LOG.log(Level.INFO, "Successfully confirmation - E-mail: " + email.getEmail());
+				
+				String messageSubject = "Locadora de Veículos BH - E-mail confirmado";
+				
+				String messageText = "Obrigado por confirmar sua conta. \n\n"
+						+ "Faça login e realize a reserva do seu veículo agora mesmo. \n\n"
+						+ "http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com"
+						+ "\n\nVocê pode reservar no site e retirar na agência mais próxima de você. \nConfira as melhores condições para você reservar e aproveitar!";
+				
+				EmailConfirmation sendEmail = new EmailConfirmation();
+				sendEmail.confirmation(email.getEmail(), messageSubject, messageText);
+				
+				LOG.exiting(NAME, "confirmation");
+				return true;
+			}
+			else {
+				LOG.exiting(NAME, "confirmation");
+				return false;
+			}
 		}
 		catch (SQLException e) {
 			LOG.log(Level.SEVERE, "Failed confirmation - E-mail: " + email.getEmail() + " - Erro: " + e);
