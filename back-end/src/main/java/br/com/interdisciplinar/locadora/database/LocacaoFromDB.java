5fe98f31f767ab5324bb88ed85abb2a2d3874941
Humberto <humbertoodantas@gmail.com>
2021-09-25 23:02:30 -0300

    Bug fix

--- back-end/src/main/java/br/com/interdisciplinar/locadora/database/LocacaoFromDB.java
+++ back-end/src/main/java/br/com/interdisciplinar/locadora/database/LocacaoFromDB.java
@@ -182,7 +182,7 @@ public class LocacaoFromDB {
 						+ "  </div>\n"
 						+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
 						+ "    <div class=\"btn btn--flat btn--large\" style=\"Margin-bottom: 20px;text-align: center;\">\n"
-						+ "      <a style=\"border-radius: 4px;display: inline-block;font-size: 14px;font-weight: bold;line-height: 24px;padding: 12px 24px;text-align: center;text-decoration: none !important;transition: opacity 0.1s ease-in;color: #ffffff !important;background-color: #337ab7;font-family: sans-serif;\" href=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com/src/pages/confirmation.html?e=" + "\" target=\"_blank\">\n"
+						+ "      <a style=\"border-radius: 4px;display: inline-block;font-size: 14px;font-weight: bold;line-height: 24px;padding: 12px 24px;text-align: center;text-decoration: none !important;transition: opacity 0.1s ease-in;color: #ffffff !important;background-color: #337ab7;font-family: sans-serif;\" href=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com/src/pages/detalhes.html?u=" + locacao.getCpf_locatario() + "\" target=\"_blank\">\n"
 						+ "        Ver detalhes\n"
 						+ "      </a>\n"
 						+ "  </div>\n"
@@ -259,6 +259,13 @@ public class LocacaoFromDB {
 		String sql3 = EnvVariables.getEnvVariable("DATABASE_CANCEL_LOCATION_3");
 		
 		boolean check = false;
+		String email = "";
+		String nome = ""; 
+		String modelo = "";
+		String pagamento_no_site = "";
+		String cartao_pagamento = "";
+		String valor_total_a_pagar = "";
+		String texto = "Sua reserva foi cancelada e nenhum valor será cobrado por isso. Estamos ansiosos para sua próxima reserva";
 		
 		try {
 			PreparedStatement CHECK_LOCATARIO_ACTIVE = Database.connect().prepareStatement(CHECK_LOCATARIO);
@@ -278,12 +285,18 @@ public class LocacaoFromDB {
 			while(f1.next()) {
 				if(f1.getString(15).equals("0")) {
 					valid1 = true;
+					modelo = f1.getString(6);
+					pagamento_no_site = f1.getString(20);
+					cartao_pagamento = f1.getString(21);
+					valor_total_a_pagar = f1.getString(15);
 				}
 			}
 			
 			while(f2.next()) {
 				if(f2.getString(22).equals("1")) {
 					valid2 = true;
+					nome = f2.getString(1);
+					email = f2.getString(6);
 				}
 			}
 			
@@ -310,6 +323,58 @@ public class LocacaoFromDB {
 				
 				check = true;
 				LOG.log(Level.INFO, "Location deleted from the database - Location ID: " + location.getIdLocacao() + " - User CPF: " + location.getCpf());
+				
+				String[] nomeSeparado = nome.split(" ");
+				
+				String messageSubject = "Locadora de Veículos BH - " + modelo + " - Reserva cancelada";
+				
+				if(pagamento_no_site.equals("true")) {
+					texto = "Você já pagou pela reserva, mas pode deixar que cuidaremos de tudo pra você! <br><br>Faremos o estorno do valor de " + valor_total_a_pagar + " para o cartão de final " + cartao_pagamento + " e o valor estará disponível na sua fatura em até 10 dias úteis! ";
+				}
+				
+				String messageText = "<!DOCTYPE html>\n"
+						+ "<html lang=\"pt-br\">\n"
+						+ "<head>\n"
+						+ "  <meta charset=\"UTF-8\">\n"
+						+ "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
+						+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+						+ "  <title>" + messageSubject + "</title>\n"
+						+ "</head>\n"
+						+ "<body>\n"
+						+ "  <div role=\"banner\">\n"
+						+ "    <div class=\"header\" style=\"Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);\" id=\"emb-email-header-container\">\n"
+						+ "      <div class=\"logo emb-logo-margin-box\" style=\"font-size: 26px;line-height: 32px;Margin-top: 16px;Margin-bottom: 32px;color: #41637e;font-family: sans-serif;Margin-left: 20px;Margin-right: 20px;\" align=\"center\">\n"
+						+ "        <div class=\"logo-center\" align=\"center\" id=\"emb-email-header\"><img style=\"display: block;height: auto;width: 100%;border: 0;max-width: 141px;\" src=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com/src/img/favicon.png\" alt width=\"141\"></div>\n"
+						+ "      </div>\n"
+						+ "    </div>\n"
+						+ "  </div>\n"
+						+ "  <div>\n"
+						+ "    <div class=\"layout one-col fixed-width stack\" style=\"Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;\">\n"
+						+ "    <div class=\"layout__inner\" style=\"border-collapse: collapse;display: table;width: 100%;background-color: #ffffff;\">\n"
+						+ "    <div class=\"column\" style=\"text-align: left;color: #717a8a;font-size: 16px;line-height: 24px;font-family: sans-serif;\">\n"
+						+ "    <div style=\"Margin-left: 20px;Margin-right: 20px;Margin-top: 24px;\">\n"
+						+ "  </div>\n"
+						+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+						+ "    <h1 style=\"Margin-top: 0;Margin-bottom: 20px;font-style: normal;font-weight: normal;color: #3d3b3d;font-size: 30px;line-height: 38px;text-align: center;\">\n"
+						+ "      " + "Olá, " + nomeSeparado[0] + "! Você cancelou sua reserva pelo site! Sabemos que imprevistos acontecem e sentimos muito por isso" + "\n"
+						+ "    </h1>\n"
+						+ "  </div>\n"
+						+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+						+ "    <h2 class=\"size-24\" style=\"Margin-top: 0;Margin-bottom: 16px;font-style: normal;font-weight: normal;color: #3d3b3d;font-size: 20px;line-height: 28px;text-align: center;\" lang=\"x-size-24\">\n"
+						+ "      " + texto + "" + "<br><br>\n"
+						+ "    </h2>\n"
+						+ "  </div>\n"
+						+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+						+ "    <div class=\"btn btn--flat btn--large\" style=\"Margin-bottom: 20px;text-align: center;\">\n"
+						+ "      <a style=\"border-radius: 4px;display: inline-block;font-size: 14px;font-weight: bold;line-height: 24px;padding: 12px 24px;text-align: center;text-decoration: none !important;transition: opacity 0.1s ease-in;color: #ffffff !important;background-color: #337ab7;font-family: sans-serif;\" href=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com" + "\" target=\"_blank\">\n"
+						+ "        Ver outros veículos\n"
+						+ "      </a>\n"
+						+ "  </div>\n"
+						+ "</body>\n"
+						+ "</html>";
+				
+				EmailConfirmation sendEmail = new EmailConfirmation();
+				sendEmail.confirmation(email, messageSubject, messageText);
 			}
 		}
 		catch (SQLException e) {
@@ -354,6 +419,52 @@ public class LocacaoFromDB {
 			
 			check = true;
 			LOG.log(Level.INFO, "Location updated from the database - User CPF: " + location.getCpf_locatario() + " - Cartão: " + location.getCartao_pagamento() + " - pagamento_no_site: " + pagamento_no_site);
+			
+			String messageSubject = "Locadora de Veículos BH - Recebemos seu pagamento";
+			
+			String messageText = "<!DOCTYPE html>\n"
+					+ "<html lang=\"pt-br\">\n"
+					+ "<head>\n"
+					+ "  <meta charset=\"UTF-8\">\n"
+					+ "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
+					+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+					+ "  <title>" + messageSubject + "</title>\n"
+					+ "</head>\n"
+					+ "<body>\n"
+					+ "  <div role=\"banner\">\n"
+					+ "    <div class=\"header\" style=\"Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);\" id=\"emb-email-header-container\">\n"
+					+ "      <div class=\"logo emb-logo-margin-box\" style=\"font-size: 26px;line-height: 32px;Margin-top: 16px;Margin-bottom: 32px;color: #41637e;font-family: sans-serif;Margin-left: 20px;Margin-right: 20px;\" align=\"center\">\n"
+					+ "        <div class=\"logo-center\" align=\"center\" id=\"emb-email-header\"><img style=\"display: block;height: auto;width: 100%;border: 0;max-width: 141px;\" src=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com/src/img/favicon.png\" alt width=\"141\"></div>\n"
+					+ "      </div>\n"
+					+ "    </div>\n"
+					+ "  </div>\n"
+					+ "  <div>\n"
+					+ "    <div class=\"layout one-col fixed-width stack\" style=\"Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;\">\n"
+					+ "    <div class=\"layout__inner\" style=\"border-collapse: collapse;display: table;width: 100%;background-color: #ffffff;\">\n"
+					+ "    <div class=\"column\" style=\"text-align: left;color: #717a8a;font-size: 16px;line-height: 24px;font-family: sans-serif;\">\n"
+					+ "    <div style=\"Margin-left: 20px;Margin-right: 20px;Margin-top: 24px;\">\n"
+					+ "  </div>\n"
+					+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+					+ "    <h1 style=\"Margin-top: 0;Margin-bottom: 20px;font-style: normal;font-weight: normal;color: #3d3b3d;font-size: 30px;line-height: 38px;text-align: center;\">\n"
+					+ "      " + "Você realizou o pagamento da sua reserva pelo site. Obrigado!" + "\n"
+					+ "    </h1>\n"
+					+ "  </div>\n"
+					+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+					+ "    <h2 class=\"size-24\" style=\"Margin-top: 0;Margin-bottom: 16px;font-style: normal;font-weight: normal;color: #3d3b3d;font-size: 20px;line-height: 28px;text-align: center;\" lang=\"x-size-24\">\n"
+					+ "      " + "Agora é só retirar seu veículo na agência e data escolhida por você" + "" + "<br><br>\n"
+					+ "    </h2>\n"
+					+ "  </div>\n"
+					+ "  <div style=\"Margin-left: 20px;Margin-right: 20px;\">\n"
+					+ "    <div class=\"btn btn--flat btn--large\" style=\"Margin-bottom: 20px;text-align: center;\">\n"
+					+ "      <a style=\"border-radius: 4px;display: inline-block;font-size: 14px;font-weight: bold;line-height: 24px;padding: 12px 24px;text-align: center;text-decoration: none !important;transition: opacity 0.1s ease-in;color: #ffffff !important;background-color: #337ab7;font-family: sans-serif;\" href=\"http://ec2-18-119-13-255.us-east-2.compute.amazonaws.com" + "\" target=\"_blank\">\n"
+					+ "        Detalhes\n"
+					+ "      </a>\n"
+					+ "  </div>\n"
+					+ "</body>\n"
+					+ "</html>";
+			
+//			EmailConfirmation sendEmail = new EmailConfirmation();
+//			sendEmail.confirmation(email, messageSubject, messageText);
 		}
 		catch (SQLException e) {
 			check = false;
