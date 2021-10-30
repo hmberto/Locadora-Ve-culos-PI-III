6b48c7dc55c6f8882b0ad89ef479eaffbdd3dfbb
Humberto <humbertoodantas@gmail.com>
2021-10-29 21:17:37 -0300

    FAQ page

--- front-end/src/js/perguntas-frequentes.js
+++ front-end/src/js/perguntas-frequentes.js
@@ -1,51 +1,37 @@
 var loading = document.getElementById("lding");
+var seta = document.querySelectorAll(".img-seta");
+var showTxt = document.querySelectorAll(".txt-qst-1");
+var questionBox = document.querySelectorAll(".question-box");
 
-function searchFabricantes() {
+function perguntasFrequentes() {
+  loading.classList.add("hideloading");
   var session = window.localStorage.getItem("session");
   if(session != null) {
     if(session.length == 50) {
       ifLogged(session);
     }
   }
-  
-  loading.classList.remove("hideloading");
-  var text = document.querySelector(".txt");
-
-  var url = "http://3.144.171.211:8186/LocadoraVeiculos/veiculos/marcas";
-  
-  var marcas = new XMLHttpRequest();
-  marcas.open("GET", url, true);
-  marcas.send();
-
-  marcas.addEventListener('loadend', () => {
-    loading.classList.add("hideloading");
-    var div = document.getElementById("marcas");
-    if (marcas.status == 200) {
-      var json = JSON.parse(marcas.response);
-      var tamanho = Object.keys(Object.keys(json['data'])).length;
-
-      text.innerHTML="Pesquise pelo fabricante do carro: " + tamanho + " fabricantes encontradas";
-      
-      for(i = 0; i < tamanho; i++) {
-        var marca = json['data']['marca' + i]['marca'];
-
-        var li = document.createElement("li");
-        li.classList.add("font-black");
-        li.innerHTML=marca
-
-        var a = document.createElement("a");
-        a.setAttribute("href", "/src/pages/pormarca.html?marca=" + marca);
-        a.innerHTML="Ver carros"
-        a.classList.add("font-green");
-
-        div.appendChild(li);
-        div.appendChild(a);
-      }
-    }
-    else {
-      document.getElementById("erro").classList.remove("esconde-erro")
-    }
-  });
 }
 
-searchFabricantes();
+perguntasFrequentes();
+
+function rotateSeta() {
+  for(i=0; i < seta.length; i++) {
+    (function(index){
+      questionBox[i].addEventListener("click", function(){
+        if(seta[index].classList.contains("prolado")) {
+          seta[index].classList.remove("prolado");
+          showTxt[index].classList.remove("hide-txt");
+          showTxt[index].classList.add("show-txt");
+        }
+        else {
+          seta[index].classList.add("prolado");
+          showTxt[index].classList.remove("show-txt");
+          showTxt[index].classList.add("hide-txt");
+        }
+      });
+    })(i);
+  }
+}
+
+rotateSeta();
